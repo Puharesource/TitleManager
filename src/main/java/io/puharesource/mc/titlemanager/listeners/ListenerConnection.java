937d6699237789e12f6c66c599cd577ffdc10bd9
Tarkan Nielsen <Puharesource@MineActivity.com>
2014-09-06 12:50:46 +0200

    Added playername to welcome and tabmenu

--- src/main/java/io/puharesource/mc/titlemanager/listeners/ListenerConnection.java
+++ src/main/java/io/puharesource/mc/titlemanager/listeners/ListenerConnection.java
@@ -2,6 +2,8 @@ package io.puharesource.mc.titlemanager.listeners;
 
 import io.puharesource.mc.titlemanager.Config;
 import io.puharesource.mc.titlemanager.api.TabTitleCache;
+import io.puharesource.mc.titlemanager.api.TabTitleObject;
+import io.puharesource.mc.titlemanager.api.TitleObject;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
@@ -13,8 +15,13 @@ public class ListenerConnection implements Listener {
     @EventHandler
     public void onJoin(PlayerJoinEvent event) {
         if (Config.isUsingConfig()) {
-            Config.getWelcomeObject().send(event.getPlayer());
-            Config.getTabTitleObject().send(event.getPlayer());
+            TitleObject titleObject = Config.getWelcomeObject();
+            titleObject.setTitle(titleObject.getTitle().replaceAll("(?i)\\{PLAYER\\}", event.getPlayer().getName()));
+            TabTitleObject tabTitleObject = Config.getTabTitleObject();
+            tabTitleObject.setFooter(tabTitleObject.getFooter().replaceAll("(?i)\\{PLAYER\\}", event.getPlayer().getName()));
+            tabTitleObject.setHeader(tabTitleObject.getHeader().replaceAll("(?i)\\{PLAYER\\}", event.getPlayer().getName()));
+            titleObject.send(event.getPlayer());
+            tabTitleObject.send(event.getPlayer());
         }
     }
 
