4c805285458c0d10305292d3af5bfa94e9977ab4
Tarkan Nielsen <Puharesource@MineActivity.com>
2014-09-09 19:18:57 +0200

    1.0.7
    + Added ability to disable welcome message.
    + Added ability to disable tabmenu header / footer.
    + Added config update from 1.0.6 to 1.0.7

--- src/main/java/io/puharesource/mc/titlemanager/Config.java
+++ src/main/java/io/puharesource/mc/titlemanager/Config.java
@@ -16,6 +16,8 @@ import java.nio.file.Files;
 public class Config {
 
     private static boolean usingConfig;
+    private static boolean tabmenuEnabled;
+    private static boolean welcomeMessageEnabled;
 
     private static TitleObject welcomeObject;
     private static TabTitleObject tabTitleObject;
@@ -49,24 +51,52 @@ public class Config {
             fileConfig.save(config);
             reloadConfig();
         }
+
+        //Updates the config from v1.0.6 to v1.0.7
+        if(!getConfig().contains("tabmenu.enabled") || !getConfig().contains("welcome_message.enabled")) {
+            FileConfiguration oldConfig = YamlConfiguration.loadConfiguration(config);
+
+            Files.copy(config.toPath(), new File(plugin.getDataFolder(), "1.0.6-old-config.yml").toPath());
+            Files.delete(config.toPath());
+            if(!config.exists())
+                Files.copy(plugin.getResource("config.yml"), config.toPath());
+            FileConfiguration fileConfig = YamlConfiguration.loadConfiguration(new File(plugin.getDataFolder(), "config.yml"));
+
+            fileConfig.set("tabmenu.header", oldConfig.getString("tabmenu.header"));
+            fileConfig.set("tabmenu.footer", oldConfig.getString("tabmenu.footer"));
+
+            fileConfig.set("welcome_message.title", oldConfig.getString("welcome_message.title"));
+            fileConfig.set("welcome_message.subtitle", oldConfig.getString("welcome_message.subtitle"));
+            fileConfig.set("welcome_message.fadeIn", oldConfig.getInt("welcome_message.fadeIn"));
+            fileConfig.set("welcome_message.stay", oldConfig.getInt("welcome_message.stay"));
+            fileConfig.set("welcome_message.fadeOut", oldConfig.getInt("welcome_message.fadeOut"));
+
+            fileConfig.save(config);
+            reloadConfig();
+        }
+
         loadSettings();
     }
 
     static void loadSettings() {
         usingConfig = getConfig().getBoolean("usingConfig");
+        tabmenuEnabled = getConfig().getBoolean("tabmenu.enabled");
+        welcomeMessageEnabled = getConfig().getBoolean("welcome_message.enabled");
 
-        tabTitleObject = new TabTitleObject(ChatColor.translateAlternateColorCodes('&', getConfig().getString("tabmenu.header")), ChatColor.translateAlternateColorCodes('&', getConfig().getString("tabmenu.footer")));
-        welcomeObject = new TitleObject(ChatColor.translateAlternateColorCodes('&', getConfig().getString("welcome_message.title")), ChatColor.translateAlternateColorCodes('&', getConfig().getString("welcome_message.subtitle")))
-                .setFadeIn(getConfig().getInt("welcome_message.fadeIn")).setStay(getConfig().getInt("welcome_message.stay")).setFadeOut(getConfig().getInt("welcome_message.fadeOut"));
-
-        for(Player player : Bukkit.getOnlinePlayers()) {
-            TabTitleObject tabObject = tabTitleObject;
-            if(tabObject.getHeader() != null)
-                tabObject.setHeader(TextConverter.setPlayerName(player, tabObject.getHeader()));
-            if(tabObject.getFooter() != null)
-                tabObject.setFooter(TextConverter.setPlayerName(player, tabObject.getFooter()));
-            tabTitleObject.send(player);
+        if(tabmenuEnabled) {
+            tabTitleObject = new TabTitleObject(ChatColor.translateAlternateColorCodes('&', getConfig().getString("tabmenu.header")), ChatColor.translateAlternateColorCodes('&', getConfig().getString("tabmenu.footer")));
+            for(Player player : Bukkit.getOnlinePlayers()) {
+                TabTitleObject tabObject = tabTitleObject;
+                if(tabObject.getHeader() != null)
+                    tabObject.setHeader(TextConverter.setPlayerName(player, tabObject.getHeader()));
+                if(tabObject.getFooter() != null)
+                    tabObject.setFooter(TextConverter.setPlayerName(player, tabObject.getFooter()));
+                tabTitleObject.send(player);
+            }
         }
+        if(welcomeMessageEnabled)
+            welcomeObject = new TitleObject(ChatColor.translateAlternateColorCodes('&', getConfig().getString("welcome_message.title")), ChatColor.translateAlternateColorCodes('&', getConfig().getString("welcome_message.subtitle")))
+                    .setFadeIn(getConfig().getInt("welcome_message.fadeIn")).setStay(getConfig().getInt("welcome_message.stay")).setFadeOut(getConfig().getInt("welcome_message.fadeOut"));
     }
 
     public static void reloadConfig() {
@@ -92,5 +122,13 @@ public class Config {
 
     public static TabTitleObject getTabTitleObject() {
         return tabTitleObject;
+    }
+
+    public static boolean isTabmenuEnabled() {
+        return tabmenuEnabled;
+    }
+
+    public static boolean isWelcomeMessageEnabled() {
+        return welcomeMessageEnabled;
     }
 }
--- src/main/java/io/puharesource/mc/titlemanager/listeners/ListenerConnection.java
+++ src/main/java/io/puharesource/mc/titlemanager/listeners/ListenerConnection.java
@@ -18,22 +18,27 @@ public class ListenerConnection implements Listener {
     public void onJoin(PlayerJoinEvent event) {
         Player player = event.getPlayer();
         if (Config.isUsingConfig()) {
-            TitleObject titleObject = new TitleObject(Config.getWelcomeObject().getTitle(), Config.getWelcomeObject().getSubtitle())
-                    .setFadeIn(Config.getWelcomeObject().getFadeIn()).setStay(Config.getWelcomeObject().getStay()).setFadeOut(Config.getWelcomeObject().getFadeOut());
-            TabTitleObject tabTitleObject = new TabTitleObject(Config.getTabTitleObject().getHeader(), Config.getTabTitleObject().getFooter());
+            if(Config.isWelcomeMessageEnabled()) {
+                TitleObject titleObject = new TitleObject(Config.getWelcomeObject().getTitle(), Config.getWelcomeObject().getSubtitle())
+                        .setFadeIn(Config.getWelcomeObject().getFadeIn()).setStay(Config.getWelcomeObject().getStay()).setFadeOut(Config.getWelcomeObject().getFadeOut());
+                if(titleObject.getTitle() != null)
+                    titleObject.setTitle(TextConverter.setPlayerName(player, titleObject.getTitle()));
+                if(titleObject.getSubtitle() != null)
+                    titleObject.setSubtitle(TextConverter.setPlayerName(player, titleObject.getSubtitle()));
+                titleObject.send(player);
+            }
+            if(Config.isTabmenuEnabled()) {
+                TabTitleObject tabTitleObject = new TabTitleObject(Config.getTabTitleObject().getHeader(), Config.getTabTitleObject().getFooter());
 
-            if(titleObject.getTitle() != null)
-                titleObject.setTitle(TextConverter.setPlayerName(player, titleObject.getTitle()));
-            if(titleObject.getSubtitle() != null)
-                titleObject.setSubtitle(TextConverter.setPlayerName(player, titleObject.getSubtitle()));
 
-            if(tabTitleObject.getHeader() != null)
-                tabTitleObject.setHeader(TextConverter.setPlayerName(player, tabTitleObject.getHeader()));
-            if(tabTitleObject.getFooter() != null)
-                tabTitleObject.setFooter(TextConverter.setPlayerName(player, tabTitleObject.getFooter()));
+                if(tabTitleObject.getHeader() != null)
+                    tabTitleObject.setHeader(TextConverter.setPlayerName(player, tabTitleObject.getHeader()));
+                if(tabTitleObject.getFooter() != null)
+                    tabTitleObject.setFooter(TextConverter.setPlayerName(player, tabTitleObject.getFooter()));
 
-            titleObject.send(player);
-            tabTitleObject.send(player);
+
+                tabTitleObject.send(player);
+            }
         }
     }
 
