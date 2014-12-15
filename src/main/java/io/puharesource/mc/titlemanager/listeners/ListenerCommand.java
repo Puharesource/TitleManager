05f82f487268d8537764745087b2305b101c2cc7
Wundero <sublimecraftftw@gmail.com>
2014-12-15 17:45:00 -0500

    Command listener woooo

---
+++ src/main/java/io/puharesource/mc/titlemanager/listeners/ListenerCommand.java
@@ -0,0 +1,43 @@
+package io.puharesource.mc.titlemanager.listeners;
+
+import io.puharesource.mc.titlemanager.Config;
+import io.puharesource.mc.titlemanager.TitleManager;
+import io.puharesource.mc.titlemanager.api.TabTitleCache;
+import io.puharesource.mc.titlemanager.api.TabTitleObject;
+import io.puharesource.mc.titlemanager.api.TitleObject;
+import io.puharesource.mc.titlemanager.api.animations.TabTitleAnimation;
+import io.puharesource.mc.titlemanager.api.animations.TitleAnimation;
+import org.bukkit.Bukkit;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerCommandPreprocessEvent;
+
+public class ListenerCommand implements Listener {
+    @EventHandler(priority = EventPriority.MONITOR)
+    public void onCommandSend(final PlayerCommandPreprocessEvent event) {
+        if(Config.getCommandTitle(event.getMessage())!=null)
+        {
+            List<Object> messages = Config.getCommandTitle(event.getMessage);
+            for(Object obj : messages)
+            {
+                if(obj instanceof TitleObject)
+                {
+                    ((TitleObject) obj).send(event.getPlayer());
+                }
+                if(obj instanceof ActionbarTitleObject)
+                {
+                    ((ActionbarTitleObject) obj).send(event.getPlayer());
+                }
+                if(obj instanceof ActionbarTitleAnimation)
+                {
+                    ((ActionbarTitleAnimation) obj).send(event.getPlayer());
+                }
+                if(obj instanceof TitleAnimation)
+                {
+                    ((TitleAnimation) obj).send(event.getPlayer());
+                }
+            }
+        }
+    }
+}
