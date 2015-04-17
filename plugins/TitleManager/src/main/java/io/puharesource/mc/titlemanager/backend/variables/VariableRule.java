27273d54e67005151297ad88a3ca2e89ab32080f
Tarkan Nielsen <Puharesource@MineActivity.com>
2015-04-17 20:14:11 +0200

    Welp that didn't seem to be removed yet

--- plugins/TitleManager/src/main/java/io/puharesource/mc/titlemanager/backend/variables/VariableRule.java
+++
@@ -1,9 +0,0 @@
-package io.puharesource.mc.titlemanager.backend.variables;
-
-import org.bukkit.entity.Player;
-
-public abstract class VariableRule {
-    public abstract boolean rule(Player player);
-
-    public abstract String[] replace(Player player, String text);
-}
