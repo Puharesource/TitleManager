37fb372c5d4d8a6e5593e457540b8af332440eec
Tarkan Nielsen <Puharesource@MineActivity.com>
2015-04-18 22:01:27 +0200

    Actionbar messages are working again

--- plugins/TitleManager/src/main/java/io/puharesource/mc/titlemanager/backend/packet/ActionbarTitlePacket.java
+++ plugins/TitleManager/src/main/java/io/puharesource/mc/titlemanager/backend/packet/ActionbarTitlePacket.java
@@ -1,16 +1,24 @@
 package io.puharesource.mc.titlemanager.backend.packet;
 
 import io.puharesource.mc.titlemanager.TitleManager;
+import io.puharesource.mc.titlemanager.backend.reflections.ReflectionClass;
 import io.puharesource.mc.titlemanager.backend.reflections.ReflectionManager;
 
 import java.lang.reflect.InvocationTargetException;
+import java.util.Map;
 
 public final class ActionbarTitlePacket extends Packet {
+    private Object handle;
+
     public ActionbarTitlePacket(String text) {
         ReflectionManager manager = TitleManager.getInstance().getReflectionManager();
 
         try {
-            handle = manager.getClasses().get("PacketPlayOutChat").createInstance(manager.getIChatBaseComponent(text), (byte) 2);
+            Map<String, ReflectionClass> classes = manager.getClasses();
+
+            handle = classes.get("PacketPlayOutChat").
+                    getConstructor(classes.get("IChatBaseComponent").getHandle(), Byte.TYPE)
+                    .newInstance(manager.getIChatBaseComponent(text), (byte) 2);
         } catch (NoSuchMethodException | IllegalAccessException | InstantiationException | InvocationTargetException e) {
             e.printStackTrace();
         }
@@ -20,6 +28,4 @@ public final class ActionbarTitlePacket extends Packet {
     public Object getHandle() {
         return handle;
     }
-
-    private Object handle;
 }
