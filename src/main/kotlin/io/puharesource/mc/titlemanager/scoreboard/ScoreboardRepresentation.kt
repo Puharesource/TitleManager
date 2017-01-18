5352cca723d37d14d080e695c8f3d24256e01a60
Puharesource <puharesource@mineactivity.com>
2017-01-19 00:30:03 +0100

    2.0.5 - Scoreboard issues when joining fixed

--- src/main/kotlin/io/puharesource/mc/titlemanager/scoreboard/ScoreboardRepresentation.kt
+++ src/main/kotlin/io/puharesource/mc/titlemanager/scoreboard/ScoreboardRepresentation.kt
@@ -1,16 +1,20 @@
 package io.puharesource.mc.titlemanager.scoreboard
 
+import java.math.BigInteger
+import java.util.Random
 import java.util.concurrent.ConcurrentHashMap
 import java.util.concurrent.atomic.AtomicBoolean
 
 class ScoreboardRepresentation(var title: String = "", private val lines: MutableMap<Int, String> = ConcurrentHashMap()) {
+    private val random = Random()
+
     val isUpdatePending = AtomicBoolean(false)
     val isUsingPrimaryBoard = AtomicBoolean(true)
 
-    var name : String = System.nanoTime().toString()
+    var name : String = BigInteger(80, random).toString(32)
 
     fun generateNewScoreboardName() {
-        name = System.nanoTime().toString()
+        name = BigInteger(80, random).toString(32)
     }
 
     val size : Int
